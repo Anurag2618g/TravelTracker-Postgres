cf65aa0c694caea3581a2c22949b6621314726d4
Anurag2618g <anurag2618g@gmail.com>
2024-06-23 21:21:50 +0530

    Connect postgres database

--- index.js
+++ index.js
@@ -25,11 +25,26 @@ app.get("/", async (req, res) => {
   data.rows.forEach((country) => {
     countries.push(country.country_code);
   });
+
   res.render("index.ejs" , {
     total: countries.length,
-    countries: countries,
+    countries: countries
   });
 });
+
+app.post("/add", async(req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [input]);
+  
+  if (result.rows.length !== 0) {
+    const countryCode = result.rows[0].country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",[countryCode]);
+    res.redirect("/");
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
