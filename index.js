6c065a270346396eee3756d949fa69886f6a7f3d
Anurag2618g <anurag2618g@gmail.com>
2024-06-23 16:21:18 +0530

    add postgres server

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "chained_86",
+  post: 5432
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -7,9 +18,17 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// HOME PAGE //
 app.get("/", async (req, res) => {
-  //Write your code here.
-  res.render("index.ejs");
+  const data = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  data.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  res.render("index.ejs" , {
+    total: countries.length,
+    countries: countries,
+  });
 });
 
 app.listen(port, () => {
